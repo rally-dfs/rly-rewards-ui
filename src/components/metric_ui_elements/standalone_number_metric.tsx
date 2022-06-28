e1dbd77eb2b8a323b387ead59a7170be0e4cd9eb
Ryan Weald <ryan@weald.com>
2022-06-28 16:47:39 -0700

    Pull inline styles into dedicated CSS module

--- src/components/metric_ui_elements/standalone_number_metric.tsx
+++ src/components/metric_ui_elements/standalone_number_metric.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { humanReadableNumber } from '../../human_readable_number';
 
+import Styles from '../../styles/standalone_number_metric.module.css';
+
 type StandaloneNumberMetricProps = {
   metric: number;
 };
 const StandaloneNumberMetric = ({ metric }: StandaloneNumberMetricProps) => {
   return (
-    <div style={{ fontSize: 72, marginTop: 12 }}>
+    <div className={Styles.standalone_number_metric}>
       {humanReadableNumber(metric)}
     </div>
   );
