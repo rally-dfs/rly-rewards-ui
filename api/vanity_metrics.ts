ec0ac351c4d00c9e9b82c4cc5011e30bbbd3bf49
Ryan Weald <ryan@weald.com>
2022-06-29 15:19:33 -0700

    Set proper content type on proxy response

--- api/vanity_metrics.ts
+++ api/vanity_metrics.ts
@@ -2,10 +2,13 @@ import superagent from 'superagent';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-  const data = await superagent.get(
+  const proxyResponse = await superagent.get(
     'http://rly-rewards-staging.us-west-1.elasticbeanstalk.com/vanity_metrics',
   );
-  response.status(200).send(data);
+  response
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .send(proxyResponse.body);
 };
 
 export default handler;
